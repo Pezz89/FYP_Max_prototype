c33bb4b5add687149c04a389fcc950f0851ba674
Sam Perry <samuel.perry89@gmail.com>
2015-09-13 08:30:48 +0100

    Added virtualenv to setup and run python scripts

--- code/run_python.sh
+++ code/run_python.sh
@@ -1,5 +1,6 @@
 PYTHONPATH="$1/code/pysource"
 export PYTHONPATH
 cd $1/code/
+source ./bin/activate
 echo `which python`
 python ./synthesize_grains.py
---
+++ code/setup_python.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+pip install virtualenv
+virtualenv ./
+source ./bin/activate
+pip install ./
